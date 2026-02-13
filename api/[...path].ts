b731867b2d7a5b64940da24b82a3ab1c348dbace
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:34:36 -0800

    Test: add serverless-http import

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,5 @@
+import serverless from 'serverless-http';
+
 export default async function handler(req: any, res: any) {
-  res.status(200).json({ step: 'require starting' });
+  res.status(200).json({ step: 'serverless imported' });
 }
