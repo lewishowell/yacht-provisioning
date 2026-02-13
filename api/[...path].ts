120526c5a20a0f19866b4b235051ef445c05ce93
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:39:39 -0800

    Wrap Express app explicitly for Vercel

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,8 @@
 import { app } from '../server/src/app.js';
 
-export default app;
+export default (req: any, res: any) => {
+  // Strip the query param that Vercel's rewrite adds
+  if (!res.headersSent) {
+    app(req, res);
+  }
+};
