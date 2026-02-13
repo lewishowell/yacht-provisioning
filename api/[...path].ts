8b12c5120cc67848336a28714482475f262d79ab
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:28:12 -0800

    Add Express app with error catching to debug imports

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,14 @@
-export default function handler(req: any, res: any) {
-  res.status(200).json({ ok: true, path: req.url });
+import serverless from 'serverless-http';
+
+let handler: any;
+
+try {
+  const { app } = require('../server/src/app');
+  handler = serverless(app);
+} catch (err: any) {
+  handler = (_req: any, res: any) => {
+    res.status(500).json({ error: err.message, stack: err.stack });
+  };
 }
+
+export default handler;
