e1eea3fe9db7eb268349a2a87a0ca8b64162839c
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:30:57 -0800

    Add step tracking to debug which import hangs

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,10 +1,15 @@
 export default async function handler(req: any, res: any) {
+  const steps: string[] = [];
   try {
+    steps.push('start');
     const serverless = require('serverless-http');
+    steps.push('serverless loaded');
     const { app } = require('../server/src/app');
+    steps.push('app loaded');
     const wrapped = serverless(app);
+    steps.push('wrapped');
     return wrapped(req, res);
   } catch (err: any) {
-    res.status(500).json({ error: err.message, stack: err.stack });
+    res.status(500).json({ error: err.message, steps });
   }
 }
