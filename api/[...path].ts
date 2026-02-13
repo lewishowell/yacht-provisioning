1e96aa39a4600504771bfff8594850084e6f5850
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:27:17 -0800

    Minimal test function to debug Vercel routing

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,20 +1,3 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-let handler: ((req: VercelRequest, res: VercelResponse) => Promise<void>) | null = null;
-let initError: Error | null = null;
-
-try {
-  const serverless = require('serverless-http');
-  const { app } = require('../server/src/app');
-  handler = serverless(app);
-} catch (err) {
-  initError = err as Error;
-}
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  if (initError) {
-    res.status(500).json({ error: 'Function init failed', message: initError.message });
-    return;
-  }
-  return handler!(req, res);
+export default function handler(req: any, res: any) {
+  res.status(200).json({ ok: true, path: req.url });
 }
