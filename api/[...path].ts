37b1e643be4a78ff0f417af4ea691173725f40f6
Lewis Howell <lewisbhowell@gmail.com>
2026-02-13 15:35:31 -0800

    Test: add app import

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,6 @@
 import serverless from 'serverless-http';
+import { app } from '../server/src/app.js';
 
 export default async function handler(req: any, res: any) {
-  res.status(200).json({ step: 'serverless imported' });
+  res.status(200).json({ step: 'app imported' });
 }
